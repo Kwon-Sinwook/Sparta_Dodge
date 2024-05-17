60993e6ca2aaf37cd973ac9d25f99457954b92d9
qwert46 <zzzrla44@naver.com>
2024-05-17 20:28:12 +0900

    Enemy generate, Destroy

--- Sparta_Dodge/Assets/Scripts/Enemy.cs
+++ Sparta_Dodge/Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     public float speed;
     public int health;
     public Sprite[] sprites;
+    private Vector2 limit = new Vector2(-12, 0);
 
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb2D;
@@ -18,7 +19,13 @@ public class Enemy : MonoBehaviour
         rb2D.velocity = Vector2.left * speed;
     }
 
-
+    private void Update()
+    {
+        if (transform.position.x < limit.x )
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
 
 
---
+++ Sparta_Dodge/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+
+        if(curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = Random.Range(0.5f, 2f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(0, 3);
+        int ranPoint = Random.Range(0, 5);
+
+        Instantiate(enemyObjs[ranEnemy],
+                    spawnPoints[ranPoint].position,
+                    Quaternion.Euler(0, 0, -90));
+    }
+}
